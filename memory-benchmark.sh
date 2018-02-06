64a03b159f20ffa5ef3a290bfbd3cbd93c03127a
Martin Ledvinka <martin.ledvinka@fel.cvut.cz>
2018-02-06 15:01:24 +0100

    Added a script for running the benchmark with memory tracking.

---
+++ memory-benchmark.sh
@@ -0,0 +1,115 @@
+#!/bin/bash
+
+JAVA=/opt/java-8-oracle/bin/java
+OUTPUT=benchmark.log
+LOGFILE=logback.xml
+WARMUPS=1
+ROUNDS=1
+
+GRAPHDB_HOME=~/Java/graphdb-free-8.4.1/
+GRAPHDB_PIDFILE=/tmp/.graphdbpid
+start_graphdb()
+{
+    ${GRAPHDB_HOME}/bin/graphdb -d -s -p ${GRAPHDB_PIDFILE};
+    sleep 20    # Sleep to give GraphDB time to start
+}
+
+stop_graphdb()
+{
+    kill $(<"${GRAPHDB_PIDFILE}")
+    sleep 2
+}
+
+restart_graphdb()
+{
+    stop_graphdb
+    start_graphdb
+}
+
+start_repository()
+{
+    start_graphdb
+}
+
+stop_repository()
+{
+    stop_graphdb
+}
+
+restart_repository()
+{
+    stop_repository
+    start_repository
+}
+
+execute_benchmark()
+{
+    cd ${1}/target
+    echo "Create..."
+    echo "*** CREATE ***"
+    ${JAVA} -jar -Dlogback.configurationFile=${LOGFILE} ${1}.jar -w ${WARMUPS} -r ${ROUNDS} -m ../../memory/${1}_create.data create
+    restart_repository
+    echo "Batch create..."
+    echo "*** BATCH CREATE ***"
+    ${JAVA} -jar -Dlogback.configurationFile=${LOGFILE} ${1}.jar -w ${WARMUPS} -r ${ROUNDS} -m ../../memory/${1}_create-batch.data create-batch
+    restart_repository
+    echo "Retrieve..."
+    echo "*** RETRIEVE ***"
+    ${JAVA} -jar -Dlogback.configurationFile=${LOGFILE} ${1}.jar -w ${WARMUPS} -r ${ROUNDS} -m ../../memory/${1}_retrieve.data retrieve
+    restart_repository
+    echo "Retrieve all..."
+    echo "*** RETRIEVE ALL ***"
+    ${JAVA} -jar -Dlogback.configurationFile=${LOGFILE} ${1}.jar -w ${WARMUPS} -r ${ROUNDS} -m ../../memory/${1}_retrieve-all.data retrieve-all
+    restart_repository
+    echo "Update..."
+    echo "*** UPDATE ***"
+    ${JAVA} -jar -Dlogback.configurationFile=${LOGFILE} ${1}.jar -w ${WARMUPS} -r ${ROUNDS} -m ../../memory/${1}_update.data update
+    restart_repository
+    echo "Delete..."
+    echo "*** DELETE ***"
+    ${JAVA} -jar -Dlogback.configurationFile=${LOGFILE} ${1}.jar -w ${WARMUPS} -r ${ROUNDS} -m ../../memory/${1}_delete.data delete
+    restart_repository
+    cd ../..
+}
+
+echo "Running benchmark..."
+start_repository
+
+#AliBaba Benchmark
+echo "Running AliBaba..."
+echo "---------------------------------------"
+echo "|               AliBaba               |"
+echo "---------------------------------------"
+execute_benchmark "alibaba-benchmark"
+
+#Empire Benchmark
+echo "Running Empire..."
+echo "---------------------------------------"
+echo "|               Empire                |"
+echo "---------------------------------------"
+execute_benchmark "empire-benchmark"
+
+# JOPA Benchmark
+echo "Running JOPA..."
+echo "---------------------------------------"
+echo "|               JOPA                  |"
+echo "---------------------------------------"
+execute_benchmark "jopa-benchmark"
+
+
+# KOMMA Benchmark
+echo "Running KOMMA..."
+echo "---------------------------------------"
+echo "|               KOMMA                 |"
+echo "---------------------------------------"
+execute_benchmark "komma-benchmark"
+
+# RDFBeans Benchmark
+echo "Running RDFBeans..."
+echo "---------------------------------------"
+echo "|               RDFBeans              |"
+echo "---------------------------------------"
+execute_benchmark "rdfbeans-benchmark"
+
+stop_repository
+echo "Benchmark finished."
